dc37616cc9293d2526da5e522b20302b3ef3fcdc
TheCursedMerchant <35909392+TheCursedMerchant@users.noreply.github.com>
2018-09-14 00:10:46 -0400

    Add files via upload

---
+++ ServletLoginDemo/src/main/java/com/revature/servlets/ProfileServlet.java
@@ -0,0 +1,49 @@
+package com.revature.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class ProfileServlet
+ */
+public class ProfileServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProfileServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		if(session != null && session.getAttribute("username")!=null)
+		{
+			//If a session exists and there is a username then we can request our dispatcher which will forward us 
+			// to the profile page
+			request.getRequestDispatcher("Profile.html").forward(request, response);;
+		}
+		else
+		{
+			//Otherwise redirect 
+			response.sendRedirect("login");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
